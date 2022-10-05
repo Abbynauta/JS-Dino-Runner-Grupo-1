e48d7555c476a2a49b2ef357ea348b868940f772
Abbynauta <abigail.suarez@fundacion-jala.org>
2022-10-05 05:26:31 -0400

    Class 2: Dinosaur (Tarea)

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,8 +1,17 @@
 import random
 from .obstacle import Obstacle
 
-class Cactus(Obstacle):
+class SmallCactus(Obstacle):
     def __init__(self, images):
         type = random.randint(0,2)
         super().__init__(images, type)
         self.rect.y = 325
+        
+class LargeCactus(Obstacle):
+    def __init__(self, images):
+        type = random.randint(0,2)
+        super().__init__(images, type)
+        self.rect.y = 300
+    
+        
+
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,6 +1,8 @@
+from random import random
 import pygame
-from dino_runner.utils.constants import SMALL_CACTUS
-from .cactus import Cactus
+from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS, BIRD
+from .cactus import SmallCactus, LargeCactus
+from .bird import Bird
 
 
 class ObstacleManager:
@@ -9,7 +11,11 @@ class ObstacleManager:
         
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            self.obstacles.append(SmallCactus(SMALL_CACTUS))
+            if len(self.obstacles)== 1:
+                self.obstacles.append(LargeCactus(LARGE_CACTUS))
+                if len(self.obstacles) == 2:
+                    self.obstacles.append(Bird(BIRD))
             
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
